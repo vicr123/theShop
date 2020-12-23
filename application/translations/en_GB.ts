9b7b86a99ea29308abc60e06b544a9bda67a738f
Atahan Reis <atahanreis@icloud.com>
2020-12-23 16:40:07 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (54 of 54 strings)
    
    Translation: theShop/theShop Desktop
    Translate-URL: https://translate.vicr123.com/projects/theshop/theshop-desktop/en_GB/

--- application/translations/en_GB.ts
+++ application/translations/en_GB.ts
@@ -1,115 +1,115 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>AppInfoPopover</name>
     <message>
         <location filename="../popovers/appinfopopover.ui" line="106"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Install</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.ui" line="117"/>
         <location filename="../popovers/appinfopopover.cpp" line="230"/>
         <source>Launch</source>
-        <translation type="unfinished"></translation>
+        <translation>Launch</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.ui" line="128"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Remove</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.ui" line="272"/>
         <source>Visit Website</source>
-        <translation type="unfinished"></translation>
+        <translation>Visit Website</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.ui" line="279"/>
         <source>Donate</source>
-        <translation type="unfinished"></translation>
+        <translation>Donate</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="143"/>
         <source>Getting ready...</source>
-        <translation type="unfinished"></translation>
+        <translation>Getting ready...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="146"/>
         <source>Removing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Removing...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="149"/>
         <source>Downloading...</source>
-        <translation type="unfinished"></translation>
+        <translation>Downloading...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="152"/>
         <source>Installing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Installing...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="155"/>
         <source>Updating...</source>
-        <translation type="unfinished"></translation>
+        <translation>Updating...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="163"/>
         <source>Finishing up...</source>
-        <translation type="unfinished"></translation>
+        <translation>Finishing up...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="166"/>
         <source>Waiting for package manager to become available...</source>
-        <translation type="unfinished"></translation>
+        <translation>Waiting for package manager to become available...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="169"/>
         <source>Waiting for authorisation...</source>
-        <translation type="unfinished"></translation>
+        <translation>Waiting for authorisation...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="172"/>
         <source>Processing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Processing...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="206"/>
         <source>Failed to install</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to install</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="207"/>
         <location filename="../popovers/appinfopopover.cpp" line="256"/>
         <source>View Details</source>
-        <translation type="unfinished"></translation>
+        <translation>View Details</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="211"/>
         <location filename="../popovers/appinfopopover.cpp" line="260"/>
         <source>Failure Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Failure Information</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="212"/>
         <source>%1 failed to install.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 failed to install.</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="228"/>
         <source>Installed Successfully</source>
-        <translation type="unfinished"></translation>
+        <translation>Installed Successfully</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="255"/>
         <source>Failed to remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to remove</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="261"/>
         <source>Failed to remove %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to remove %1.</translation>
     </message>
 </context>
 <context>
@@ -117,52 +117,52 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>theShop</source>
-        <translation type="unfinished"></translation>
+        <translation>theShop</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="84"/>
         <source>Explore</source>
-        <translation type="unfinished"></translation>
+        <translation>Explore</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="110"/>
         <source>Updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Updates</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="133"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Search</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="181"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>File Bug</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="190"/>
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Sources</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="199"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>About</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="208"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="211"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="51"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Help</translation>
     </message>
 </context>
 <context>
@@ -170,17 +170,17 @@
     <message>
         <location filename="../search/searchwidget.ui" line="41"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Search...</translation>
     </message>
     <message>
         <location filename="../search/searchwidget.ui" line="86"/>
         <source>What does your heart desire?</source>
-        <translation type="unfinished"></translation>
+        <translation>What does your heart desire?</translation>
     </message>
     <message>
         <location filename="../search/searchwidget.ui" line="109"/>
         <source>Go ahead and run a search, and we&apos;ll show you what you&apos;re looking for.</source>
-        <translation type="unfinished"></translation>
+        <translation>Go ahead and run a search, and we&apos;ll show you what you&apos;re looking for.</translation>
     </message>
 </context>
 <context>
@@ -188,8 +188,9 @@
     <message numerus="yes">
         <location filename="../updates/updatesmodel.cpp" line="53"/>
         <source>%n System Updates</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n System Update</numerusform>
+            <numerusform>%n System Updates</numerusform>
         </translation>
     </message>
 </context>
@@ -198,93 +199,93 @@
     <message>
         <location filename="../updates/updateswidget.ui" line="74"/>
         <source>Checking for updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Checking for updates</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="132"/>
         <source>You&apos;re all good!</source>
-        <translation type="unfinished"></translation>
+        <translation>You&apos;re all good!</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="142"/>
         <source>There are no pending updates to download and install.</source>
-        <translation type="unfinished"></translation>
+        <translation>There are no pending updates to download and install.</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="167"/>
         <location filename="../updates/updateswidget.ui" line="337"/>
         <source>Check Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Check Again</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="300"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>ACTIONS</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="315"/>
         <source>Download All Updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Download All Updates</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="326"/>
         <source>Reboot and install updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Reboot and install updates</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="362"/>
         <source>PROGRESS</source>
-        <translation type="unfinished"></translation>
+        <translation>PROGRESS</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="395"/>
         <source>UPDATES</source>
-        <translation type="unfinished"></translation>
+        <translation>UPDATES</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="132"/>
         <source>Getting prepared...</source>
-        <translation type="unfinished"></translation>
+        <translation>Getting prepared...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="135"/>
         <source>Downloading packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Downloading packages...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="138"/>
         <source>Installing packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Installing packages...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="141"/>
         <source>Updating packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Updating packages...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="144"/>
         <source>Finishing up...</source>
-        <translation type="unfinished"></translation>
+        <translation>Finishing up...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="147"/>
         <source>Reviewing downloaded packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Reviewing downloaded packages...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="150"/>
         <source>Waiting for other package managers...</source>
-        <translation type="unfinished"></translation>
+        <translation>Waiting for other package managers...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="153"/>
         <source>Waiting for authentication...</source>
-        <translation type="unfinished"></translation>
+        <translation>Waiting for authentication...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="156"/>
         <source>Installing updates...</source>
-        <translation type="unfinished"></translation>
+        <translation>Installing updates...</translation>
     </message>
 </context>
 <context>
@@ -292,7 +293,7 @@
     <message>
         <location filename="../main.cpp" line="37"/>
         <source>Software Store</source>
-        <translation type="unfinished"></translation>
+        <translation>Software Store</translation>
     </message>
 </context>
 </TS>
