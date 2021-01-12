feaca5da62b3eddcb5e7995c08a4c8c29444d225
Victor Tran <vicr12345@gmail.com>
2021-01-12 09:17:21 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (54 of 54 strings)
    
    Translation: theShop/theShop Desktop
    Translate-URL: https://translate.vicr123.com/projects/theshop/theshop-desktop/vi/

--- application/translations/vi_VN.ts
+++ application/translations/vi_VN.ts
@@ -1,115 +1,115 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>AppInfoPopover</name>
     <message>
         <location filename="../popovers/appinfopopover.ui" line="106"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.ui" line="117"/>
         <location filename="../popovers/appinfopopover.cpp" line="230"/>
         <source>Launch</source>
-        <translation type="unfinished"></translation>
+        <translation>Khởi chạy</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.ui" line="128"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Gỡ cài đặt</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.ui" line="272"/>
         <source>Visit Website</source>
-        <translation type="unfinished"></translation>
+        <translation>Xem trang web</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.ui" line="279"/>
         <source>Donate</source>
-        <translation type="unfinished"></translation>
+        <translation>Quyên góp</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="143"/>
         <source>Getting ready...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chuẩn bị...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="146"/>
         <source>Removing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang gỡ...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="149"/>
         <source>Downloading...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang tải xuống...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="152"/>
         <source>Installing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang cài đặt...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="155"/>
         <source>Updating...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang cập nhật...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="163"/>
         <source>Finishing up...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang hoàn tất...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="166"/>
         <source>Waiting for package manager to become available...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chờ cho trình quản lý gói sẵn sàng...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="169"/>
         <source>Waiting for authorisation...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chờ xác thực...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="172"/>
         <source>Processing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang xử lý...</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="206"/>
         <source>Failed to install</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể cài đặt</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="207"/>
         <location filename="../popovers/appinfopopover.cpp" line="256"/>
         <source>View Details</source>
-        <translation type="unfinished"></translation>
+        <translation>Xem chi tiết</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="211"/>
         <location filename="../popovers/appinfopopover.cpp" line="260"/>
         <source>Failure Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Thông tin về vụ cài đặt</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="212"/>
         <source>%1 failed to install.</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể cài đặt %1.</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="228"/>
         <source>Installed Successfully</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt thành công</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="255"/>
         <source>Failed to remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể gỡ cài đặt</translation>
     </message>
     <message>
         <location filename="../popovers/appinfopopover.cpp" line="261"/>
         <source>Failed to remove %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể gỡ %1.</translation>
     </message>
 </context>
 <context>
@@ -117,52 +117,52 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>theShop</source>
-        <translation type="unfinished"></translation>
+        <translation>theShop</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="84"/>
         <source>Explore</source>
-        <translation type="unfinished"></translation>
+        <translation>Khám phá</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="110"/>
         <source>Updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Bản cập nhật</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="133"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Tìm kiếm</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="181"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>Báo cáo lỗi</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="190"/>
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Mã nguồn</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="199"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Giới thiệu</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="208"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Thoát</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="211"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="51"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Trợ giúp</translation>
     </message>
 </context>
 <context>
@@ -170,17 +170,17 @@
     <message>
         <location filename="../search/searchwidget.ui" line="41"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Tìm kiếm...</translation>
     </message>
     <message>
         <location filename="../search/searchwidget.ui" line="86"/>
         <source>What does your heart desire?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn khao khát điều gì?</translation>
     </message>
     <message>
         <location filename="../search/searchwidget.ui" line="109"/>
         <source>Go ahead and run a search, and we&apos;ll show you what you&apos;re looking for.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hãy tìm kiếm một ứng dụng, rồi chúng tôi sẽ hiện lên điều bạn đang tìm.</translation>
     </message>
 </context>
 <context>
@@ -188,8 +188,8 @@
     <message numerus="yes">
         <location filename="../updates/updatesmodel.cpp" line="53"/>
         <source>%n System Updates</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n bản cập nhật hệ thống</numerusform>
         </translation>
     </message>
 </context>
@@ -198,93 +198,93 @@
     <message>
         <location filename="../updates/updateswidget.ui" line="74"/>
         <source>Checking for updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang kiểm tra cập nhật</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="132"/>
         <source>You&apos;re all good!</source>
-        <translation type="unfinished"></translation>
+        <translation>Ôi, tuyệt vời!</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="142"/>
         <source>There are no pending updates to download and install.</source>
-        <translation type="unfinished"></translation>
+        <translation>Không có bản cập nhật nào để tải xuống và cài đặt.</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="167"/>
         <location filename="../updates/updateswidget.ui" line="337"/>
         <source>Check Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Kiểm tra lại</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="300"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>HOẠT ĐỘNG</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="315"/>
         <source>Download All Updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Tải xuống các bản cập nhật</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="326"/>
         <source>Reboot and install updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Khởi động lại và cài bản cập nhật</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="362"/>
         <source>PROGRESS</source>
-        <translation type="unfinished"></translation>
+        <translation>TIẾN ĐỘ</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.ui" line="395"/>
         <source>UPDATES</source>
-        <translation type="unfinished"></translation>
+        <translation>BẢN CẬP NHẬT</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="132"/>
         <source>Getting prepared...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chuẩn bị...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="135"/>
         <source>Downloading packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang tải xuống gói cập nhật...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="138"/>
         <source>Installing packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang cài đặt gói cập nhật...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="141"/>
         <source>Updating packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang cài đặt gói cập nhật...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="144"/>
         <source>Finishing up...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang hoàn tất...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="147"/>
         <source>Reviewing downloaded packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang xem lại gói đã tải xuống...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="150"/>
         <source>Waiting for other package managers...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chờ cho trình quản lý gói khác...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="153"/>
         <source>Waiting for authentication...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chờ xác thực...</translation>
     </message>
     <message>
         <location filename="../updates/updateswidget.cpp" line="156"/>
         <source>Installing updates...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang cài đặt bản cập nhật...</translation>
     </message>
 </context>
 <context>
@@ -292,7 +292,7 @@
     <message>
         <location filename="../main.cpp" line="37"/>
         <source>Software Store</source>
-        <translation type="unfinished"></translation>
+        <translation>Cửa hàng ứng dụng</translation>
     </message>
 </context>
 </TS>
